d0b104dc1ab10991fcfb155db541554bd968bc76
skliarovap <iarkova9927@gmail.com>
2025-03-26 17:29:38 +1000

    Theme Commit

--- app/src/main/java/com/bloodmoon/tourbyplanets/ui/theme/Shape.kt
+++ app/src/main/java/com/bloodmoon/tourbyplanets/ui/theme/Shape.kt
@@ -1,11 +1,19 @@
 package com.bloodmoon.tourbyplanets.ui.theme
 
+import androidx.compose.foundation.shape.CornerBasedShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Shapes
 import androidx.compose.ui.unit.dp
 
-val Shapes = Shapes(
+data class JetTourByPlanets (
+    val small:CornerBasedShape,
+    val medium:CornerBasedShape,
+    val large:CornerBasedShape,
+    val dialog: CornerBasedShape
+)
+
+val Shapes = JetTourByPlanets(
     small = RoundedCornerShape(8.dp),
-    medium = RoundedCornerShape(32.dp),
-    large = RoundedCornerShape(0.dp)
+    medium = RoundedCornerShape(16.dp),
+    large = RoundedCornerShape(32.dp) ,
+    dialog = RoundedCornerShape(8.dp)
 )
