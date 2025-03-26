50282cd08085e489633db9a35bef3478d4dd5873
skliarovap <iarkova9927@gmail.com>
2025-03-26 17:10:51 +1000

    Theme Commit

--- app/src/main/java/com/bloodmoon/tourbyplanets/ui/theme/Theme.kt
+++ app/src/main/java/com/bloodmoon/tourbyplanets/ui/theme/Theme.kt
@@ -12,25 +12,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = PrimaryColor,
+    secondary = SecondaryColor,
+    tertiary = SurfaceColor
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = PrimaryColor,
+    secondary = SecondaryColor,
+    tertiary = SurfaceColor
 )
 
 @Composable
@@ -53,6 +43,7 @@ fun TourByPlanetsTheme(
     MaterialTheme(
         colorScheme = colorScheme,
         typography = Typography,
-        content = content
+        content = content,
+        shapes = Shapes
     )
 }
--- app/src/main/java/com/bloodmoon/tourbyplanets/ui/theme/Type.kt
+++ app/src/main/java/com/bloodmoon/tourbyplanets/ui/theme/Type.kt
@@ -6,6 +6,21 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
+import androidx.compose.material.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.bloodmoon.tourbyplanets.R
+
+val fonts = FontFamily(
+    Font(resId = R.font.roboto_regular, weight = FontWeight.Normal),
+    Font(resId = R.font.roboto_medium, weight = FontWeight.Medium),
+    Font(resId = R.font.roboto_bold, weight = FontWeight.Bold),
+    Font(resId = R.font.roboto_light, weight = FontWeight.Light)
+)
+
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
@@ -15,20 +30,5 @@ val Typography = Typography(
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+
 )
