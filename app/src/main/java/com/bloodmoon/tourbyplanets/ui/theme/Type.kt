cb51eca8f947e52c1bfa8b3eba4f400e42d585bf
skliarovap <iarkova9927@gmail.com>
2025-03-26 17:11:27 +1000

    Theme Commit

--- app/src/main/java/com/bloodmoon/tourbyplanets/ui/theme/Type.kt
+++ app/src/main/java/com/bloodmoon/tourbyplanets/ui/theme/Type.kt
@@ -1,17 +1,11 @@
 package com.bloodmoon.tourbyplanets.ui.theme
 
 import androidx.compose.material3.Typography
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
-
-import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
 import com.bloodmoon.tourbyplanets.R
 
 val fonts = FontFamily(
